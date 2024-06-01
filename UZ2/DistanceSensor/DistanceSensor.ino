179b2d858babf3775c77962f03e378106334ae5f
Adrian Petre <darthkojones@gmail.com>
2024-06-01 14:09:57 +0200

    distance sensor lights up 1 LED for each 10 cms of distance

--- UZ2/DistanceSensor/DistanceSensor.ino
+++ UZ2/DistanceSensor/DistanceSensor.ino
@@ -1,9 +1,6 @@
 #include <Arduino.h>
 
-// Define the pins for the RGB LED and the LED Matrix
-#define RGB_RED_PIN 3
-#define RGB_GREEN_PIN 5
-#define RGB_BLUE_PIN 6
+// Define the pins for the LED Matrix
 #define LATCH_PIN 11
 #define DATA_PIN 16
 #define CLOCK_PIN 15
@@ -35,15 +32,15 @@ class LedMatrix {
 
     void drawColumn(uint8_t column, byte mask) {
       if (column < 8) {
-        displayBuffer[7 - column] = mask;
+        displayBuffer[column] = mask;
       }
     }
 
     void update() {
       digitalWrite(this->latchPin, LOW);
       for (byte i = 0; i < 8; i++) {
-        shiftOut(this->dataPin, this->clockPin, LSBFIRST, displayBuffer[i]);
-        shiftOut(this->dataPin, this->clockPin, LSBFIRST, 1 << i);
+        shiftOut(this->dataPin, this->clockPin, MSBFIRST, displayBuffer[i]);
+        shiftOut(this->dataPin, this->clockPin, MSBFIRST, 1 << i);
       }
       digitalWrite(this->latchPin, HIGH);
     }
@@ -87,78 +84,40 @@ class DistanceSensor {
     }
 };
 
-// DibsE class
-class DibsE {
-  private:
-    DistanceSensor sensor;
-    LedMatrix& ledMatrix;
-
-  public:
-    DibsE(DistanceSensor sensor, LedMatrix& ledMatrix) : sensor(sensor), ledMatrix(ledMatrix) {}
-
-    void begin() {
-      sensor.begin();
-      // Initialize RGB LED pins as outputs
-      pinMode(RGB_RED_PIN, OUTPUT);
-      pinMode(RGB_GREEN_PIN, OUTPUT);
-      pinMode(RGB_BLUE_PIN, OUTPUT);
-      // Initialize the LED matrix
-      ledMatrix.clear();
-    }
-
-    void update() {
-      int distance = sensor.getDistance();
-    
-      // Debugging output for distance
-      Serial.print("Distance for LED Matrix: ");
-      Serial.println(distance);
-    
-      // Control RGB LED based on distance
-      int blinkRate = map(distance, 0, 200, 100, 1000); 
-      digitalWrite(RGB_RED_PIN, HIGH);
-      delay(blinkRate / 2);
-      digitalWrite(RGB_RED_PIN, LOW);
-      delay(blinkRate / 2);
-    
-      // Update LED Matrix
-      static byte previousColumns[8] = {0};
-    
-      // Shift previous columns to the right
-      for (int i = 7; i > 0; --i) {
-        previousColumns[i] = previousColumns[i - 1];
-      }
-    
-      // Map the distance to the LED matrix height (inverted)
-      byte newColumn = map(distance, 0, 70, 0, 7); 
-      if (newColumn > 7) newColumn = 7; // Ensure the column index is within bounds
-      previousColumns[0] = (1 << newColumn) - 1; // Create the mask for the column
-    
-      // Clear the display buffer
-      ledMatrix.clear();
-    
-      // Draw the updated columns on the LED matrix
-      for (int i = 0; i < 8; ++i) {
-        ledMatrix.drawColumn(i, previousColumns[i]);
-      }
-    
-      // Update the LED matrix display
-      ledMatrix.update();
-    }
-};
-
 // Initialize the LedMatrix object
 LedMatrix ledMatrix(LATCH_PIN, DATA_PIN, CLOCK_PIN);
 
-// Initialize DistanceSensor and DibsE objects
+// Initialize DistanceSensor object
 DistanceSensor sensor(TRIG_PIN, ECHO_PIN);
-DibsE dibse(sensor, ledMatrix);
 
 void setup() {
   Serial.begin(9600); // Initialize Serial Monitor
-  dibse.begin();
+  sensor.begin();
 }
 
 void loop() {
-  dibse.update();
+  int distance = sensor.getDistance();
+  
+  // Debugging output for distance
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  // Display the distance on the LED matrix
+  ledMatrix.clear();
+  
+  // Calculate mask based on distance
+  byte rowMask = 0;
+  if (distance >= 0 && distance <= 70) {
+    int rowsToLight = distance / 10 + 1; // Scale distance to number of rows
+    for (int i = 0; i < rowsToLight; i++) {
+      rowMask |= (1 << (7 - i));
+    }
+  }
+  
+  for (int col = 0; col < 8; col++) {
+    ledMatrix.drawColumn(col, rowMask);
+  }
+
+  ledMatrix.update();
   delay(1000); // Update every second
 }
